59d0566aca26b92ccb353656f4f71d4b366e975f
keshavdking <keshupoddar@gmail.com>
2020-07-31 12:31:05 +0530

    added fancy toast msg to th project using external libraries.

--- app/src/main/java/com/keshavdking/instagramclone/SignUp.java
+++ app/src/main/java/com/keshavdking/instagramclone/SignUp.java
@@ -3,13 +3,13 @@ package com.keshavdking.instagramclone;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class SignUp extends AppCompatActivity {
     private EditText name,kickSpeed,punchSpeed,punchPower,kickPower;
@@ -27,7 +27,7 @@ public class SignUp extends AppCompatActivity {
 
     public void helloWorldIsTapped(View view){
 
-        ParseObject kickBoxer = new ParseObject("KickBoxer");
+        final ParseObject kickBoxer = new ParseObject("KickBoxer");
         kickBoxer.put("name",name.getText().toString());
         kickBoxer.put("punchSpeed",Integer.parseInt(punchSpeed.getText().toString()));
         kickBoxer.put("punchPower",Integer.parseInt(punchPower.getText().toString()));
@@ -37,7 +37,9 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void done(ParseException e) {
                 if (e==null){
-                    Toast.makeText(SignUp.this,"KickBoxer Object is saved successfully",Toast.LENGTH_LONG).show();
+                    FancyToast.makeText(SignUp.this,kickBoxer.get("name")+"is added to the server",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                }else {
+                    FancyToast.makeText(SignUp.this,"Error while adding to server",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
                 }
             }
         });
